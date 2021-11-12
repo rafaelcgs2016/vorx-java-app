bf0fadbe1947e4106a8a723ab5f2bc0f2d3efdc3
rafaelcgs2016 <56272021+rafaelcgs2016@users.noreply.github.com>
2021-11-12 00:37:45 -0300

    Update deploy.sh

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,6 +2,6 @@
 echo maven-project > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
-scp -i /var/jenkins_home/secrets/labsuser.pem /tmp/.auth ec2-user@54.146.56.35:/tmp/.auth
-scp -i /var/jenkins_home/secrets/labsuser.pem ./jenkins/deploy/publish ec2-user@54.146.56.35:/tmp/publish
-ssh -i /var/jenkins_home/secrets/labsuser.pem ec2-user@54.146.56.35 "bash /tmp/publish"
+scp -i /var/jenkins_home/secrets/labsuser.pem /tmp/.auth ec2-user@10.0.2.34:/tmp/.auth
+scp -i /var/jenkins_home/secrets/labsuser.pem ./jenkins/deploy/publish ec2-user@10.0.2.34:/tmp/publish
+ssh -i /var/jenkins_home/secrets/labsuser.pem ec2-user@10.0.2.34 "bash /tmp/publish"
